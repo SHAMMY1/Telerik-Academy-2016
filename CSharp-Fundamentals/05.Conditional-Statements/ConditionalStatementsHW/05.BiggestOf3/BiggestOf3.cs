61788c9e16de9db9574fe22b9159e5cd73c0ccb4
SHAMMY <Shammy.bg1@gmail.com>
2016-04-25 21:19:34 +0300

    adsd

---
+++ CSharp-Fundamentals/05.Conditional-Statements/ConditionalStatementsHW/05.BiggestOf3/BiggestOf3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class BiggestOf3
+{
+    static void Main()
+    {
+        int numbersCount = 3;
+
+        int biggestNumber = int.MinValue;
+
+        for (int i = 0; i < numbersCount; i++)
+        {
+            int currentNumber = int.Parse(Console.ReadLine());
+
+            if (currentNumber > biggestNumber)
+            {
+                biggestNumber = currentNumber;
+            }
+        }
+
+        Console.WriteLine(biggestNumber);
+    }
+}
+
