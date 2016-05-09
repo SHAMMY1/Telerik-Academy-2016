29cd841e142eea14c48d6e88aa2cb7c503279ae3
SHAMMY1 <shammy.bg1@gmail.com>
2016-05-09 19:41:18 +0300

    zxf

---
+++ CSharp-Advanced/Homeworks/01.Arrays/ArraysHW/01.AllocateArray/AllocateArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class AllocateArray
+{
+    static void Main()
+    {
+        int arrayLength = int.Parse(Console.ReadLine());
+
+        int[] allocatedArray = new int[arrayLength];
+
+        for (int i = 0; i < allocatedArray.Length; i++)
+        {
+            allocatedArray[i] = i * 5;
+        }
+
+        Console.WriteLine(string.Join("\n", allocatedArray));
+    }
+}
+
---
+++ CSharp-Fundamentals/ExamPreparation/2015-20160-2Feb-Evening/Task02EncodingSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = int.Parse(Console.ReadLine());
+
+        int result = 0;
+        char symbol;
+
+        do
+        {
+            symbol = (char)Console.Read();
+
+            if (char.IsDigit(symbol))
+            {
+                int multipliare = int.Parse(symbol.ToString());
+                result *= multipliare;
+            }
+            else if (char.IsLetter(symbol))
+            {
+                int index = char.ToUpper(symbol) - 'A';
+                result += index;
+            }
+            else
+            {
+                result %= m;
+            }
+        } while (symbol != '@');
+
+        Console.WriteLine(result);
+    }
+}
+
